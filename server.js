f9755e7aa4dd4ac90b66192e30563364866cc056
nitin8462 <nitin.sharma8462@gmail.com>
2018-11-11 20:02:00 +0530

    setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 console.log("Server.js");
 
 const express= require('express');
+const port=process.env.PORT || 3000;
 var app= express();
 
 app.use(express.static(__dirname+"/public"))
@@ -8,4 +9,23 @@ app.get('/',(req, res)=>{
     res.send("Hello express");
 });
 
-app.listen(3000);
+app.listen(port,()=>{
+  console.log(`Server is up on port ${port}`);
+});
+
+
+// git init
+// ls -a
+// git status
+
+// git add file name
+
+//git config user.name "nitin8462"
+//git config user.email "nitin.sharma8462@gmail.com"
+// git commit -m 'initial commit '
+// ssh key gen
+// ssh-keygen -t rsa -b 4096 -C 'nitin.sharma8462@gmail.com' (generate ssh key )
+//ls -al ~/.ssh
+// eval "$(ssh-agent -s)" (agent pid)
+
+// ssh-add ~/.ssh/id_rsa
